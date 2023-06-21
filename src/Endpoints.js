920a66f2d28a70b9c73333ceb3848f51401dc9a9
Thomas Cuvillier <titiomm@gmail.com>
2023-06-21 22:26:12 +0200

    change indexer to not have dynamic err msg

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -88,7 +88,8 @@ export default class Endpoints extends Task {
             if(!response.data?.success){
                 this.errors.push("EVM Indexer status is not OK");
             } else if(response.data.secondsBehind > (process.env.MAX_RPC_BLOCK_TRAIL * 2)){
-                this.errors.push("EVM Indexer is " + response.data.secondsBehind + " seconds behind");
+                this.errors.push("EVM Indexer is out of sync with RPC ");
+                this.infos.push("EVM Indexer is behind RPC by " + response.data.secondsBehind + " seconds");
             }
         }).catch((error) => {
             console.log(error.message);
